355e910befb9d70aa676ed21a90ce35cf3d68bf5
Manel Martos <manuel.martos@esfds.com>
2018-10-24 22:47:52 +0200

    [GREEN] New line as optional separator

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -1,13 +1,34 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 class StringCalculator {
+    private static final String COMA_SEPARATOR = ",";
+    private static final String NEW_LINE_SEPARATOR = "\n";
+
     int add(String input) {
-        int result = 0;
+        return sumNumbers(getNumbers(input));
+    }
+
+    private List<String> getNumbers(String input) {
+        List<String> result = new ArrayList<>();
         if (!input.isEmpty()) {
-            String[] numbers = input.split(",");
-            for (String number : numbers) {
-                if (!number.isEmpty()) {
-                    result += Integer.valueOf(number);
+            String[] inputSplit = input.split(COMA_SEPARATOR);
+            for (String item : inputSplit) {
+                if (item.contains(NEW_LINE_SEPARATOR)) {
+                    result.addAll(Arrays.asList(item.split(NEW_LINE_SEPARATOR)));
+                } else {
+                    result.add(item);
                 }
             }
+        }
+        return result;
+    }
+
+    private int sumNumbers(List<String> numbers) {
+        int result = 0;
+        for (String number : numbers) {
+            result += Integer.valueOf(number);
         }
         return result;
     }
